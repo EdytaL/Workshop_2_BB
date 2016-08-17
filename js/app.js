dc876b94fb21df7d18ba94990b31626318120cde
EdytaL <lysiakedyta@gmail.com>
2016-08-17 15:30:00 +0200

    Fixed media_query

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 $(function() {
+    
+    //-------variables - list in section info
+    var $infoList = $(".info ul");
+    var $checkBoxMenu = $(".menu-hamburger input");
+    var $checkBoxLabel = $(".menu-hamburger label");
+    var $navItems = $(".nav-item");
+    var $container = $(".container");
+    
+    
+    //-------function responsibile for counting installment
     $(".slider1").slider({ 
         range: "max",
         min: 1000,
@@ -27,4 +37,35 @@ $(function() {
         $(".raty").text(Math.round(((value/months)*1.03)*100)/100);
     }
     calc_value_to_pay(12, 1000);
+    
+    $("#nav-icon").click(function(){
+		$(this).toggleClass('open');
+	});
+    
+    //--------event on checkbox, menu-hamburger
+    $checkBoxMenu.change(function() {
+        if(this.checked) {
+        $navItems.css({"display": "block"})
+        $container.css({
+            "left": "100px",
+            "transition": "left 0.2s"
+        });
+        } else {
+            $container.css({
+                "left": "0",
+                "transition": "left 0.2s" 
+            });
+            $navItems.css({"display": "none"})
+        }
+    })
+    
+    
+    //--------event on info box
+    $infoList.on("click", function(event) {
+        var $this = $(this);
+        var $listElements = $this.children();
+        
+        $listElements.slideToggle(1000)
+        
+    })
 })
